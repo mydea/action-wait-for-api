310e6be4b9abd798086100a15929bcf31ff6d33a
Francesco Novy <francesco@fabscale.com>
2020-10-28 13:18:14 +0100

    Wrap everything in async function

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 const core = require('@actions/core');
 const fetch = require('node-fetch');
 
-try {
-  let method = core.getInput('method');
-  let url = core.getInput('url');
-  let headersString = core.getInput('headers');
-  let timeout = parseInt(core.getInput('timeout'));
-  let interval = parseInt(core.getInput('interval'));
-  let expectedStatus = parseInt(core.getInput('expected-status'));
-  let expectedResponseField = core.getInput('expected-response-field');
-  let expectedResponseFieldValue = core.getInput(
-    'expected-response-field-value'
-  );
-
-  let headers = headersString ? JSON.parse(headersString) : {};
-  let start = +new Date();
-
-  core.debug(`=== Waiting for API response to continue. ===`);
-  core.debug(`URL: ${url}`);
-  core.debug(`Method: ${method}`);
-  core.debug(`Headers: ${headersString}`);
-  core.debug('# Waiting for this response:');
-  core.debug(`HTTP Status: ${expectedStatus}`);
-
-  if (expectedResponseField && expectedResponseFieldValue) {
-    core.debug(
-      `Response contains field "${expectedResponseField}" with value "${expectedResponseFieldValue}"`
+(async function () {
+  try {
+    let method = core.getInput('method');
+    let url = core.getInput('url');
+    let headersString = core.getInput('headers');
+    let timeout = parseInt(core.getInput('timeout'));
+    let interval = parseInt(core.getInput('interval'));
+    let expectedStatus = parseInt(core.getInput('expected-status'));
+    let expectedResponseField = core.getInput('expected-response-field');
+    let expectedResponseFieldValue = core.getInput(
+      'expected-response-field-value'
     );
-  } else if (expectedResponseField) {
-    core.debug(`Response contains field "${expectedResponseField}"`);
+
+    let headers = headersString ? JSON.parse(headersString) : {};
+    let start = +new Date();
+
+    core.debug(`=== Waiting for API response to continue. ===`);
+    core.debug(`URL: ${url}`);
+    core.debug(`Method: ${method}`);
+    core.debug(`Headers: ${headersString}`);
+    core.debug('# Waiting for this response:');
+    core.debug(`HTTP Status: ${expectedStatus}`);
+
+    if (expectedResponseField && expectedResponseFieldValue) {
+      core.debug(
+        `Response contains field "${expectedResponseField}" with value "${expectedResponseFieldValue}"`
+      );
+    } else if (expectedResponseField) {
+      core.debug(`Response contains field "${expectedResponseField}"`);
+    }
+
+    await tryFetch({
+      start,
+      interval,
+      timeout,
+      url,
+      method,
+      headers,
+      expectedStatus,
+      expectedResponseField,
+      expectedResponseFieldValue,
+    });
+
+    core.debug('API request was successfull.');
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  await tryFetch({
-    start,
-    interval,
-    timeout,
-    url,
-    method,
-    headers,
-    expectedStatus,
-    expectedResponseField,
-    expectedResponseFieldValue,
-  });
-
-  core.debug('API request was successfull.');
-} catch (error) {
-  core.setFailed(error.message);
-}
+})();
 
 async function tryFetch({
   start,
